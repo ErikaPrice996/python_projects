062eb84db099dd8a55d40554275e4b0a5e346ab6
Erika Price <108128810+ErikaPrice996@users.noreply.github.com>
2026-04-28 20:37:07 -0400

    Add files via upload
    simple quiz game to learn some OOP skills with python

---
+++ quiz-game-start/main.py
@@ -0,0 +1,15 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    question_entry = Question(question["question"], question["correct_answer"])
+    question_bank.append(question_entry)
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You've completed the quiz.")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
